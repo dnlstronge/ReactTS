9f9236496fab8eea0b5783f514846edd6c891b73
Dan Stronge <dnlstronge@gmail.com>
2023-04-14 13:22:34 +0100

    [x] - demo typing

--- basics.ts
+++ basics.ts
@@ -19,3 +19,29 @@ let answer: boolean;
 
 answer = true; // will work
 answer = "true"; // wont work
+
+/* Try with an object */
+
+let object: {
+    value1: number,
+    value2: string,
+    funct: Function
+}
+const myFunction = () => {
+    console.log("something")
+}
+const myArray = [1, 2, 3]
+
+object = {
+    value1: 55, // works
+    value2: "fiftyfive", // works
+    funct: myFunction // works
+}
+
+object = {
+    value1: "fiftyfive", // wont work
+    value2: 55 // wont work
+    funct: myArray // wont work
+}
+
+

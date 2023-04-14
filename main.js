f5b30e51cebc2ddb8eda2edfdc122237eb30c2f8
Dan Stronge <dnlstronge@gmail.com>
2023-04-14 13:54:16 +0100

    [x] - move files [x] - create generic function [x] - compile and view result in browser

---
+++ main.js
@@ -0,0 +1,5 @@
+var myfunction = function (a, b) {
+    var result = a * b;
+    console.log(result);
+};
+myfunction(5, 5); // outputs 25
---
+++ main.ts
@@ -0,0 +1,6 @@
+const myfunction = (a: number, b: number) => {
+    let result = a * b
+    console.log(result)
+}
+
+myfunction(5, 5) // outputs 25

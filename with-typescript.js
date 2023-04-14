20f5b9e0bf5aaa9a0c820fb7669938ccde826ade
Dan Stronge <dnlstronge@gmail.com>
2023-04-14 13:01:07 +0100

    [x] - compile using ts

---
+++ with-typescript.js
@@ -0,0 +1,5 @@
+var add = function (a, b) {
+    console.log(a + b);
+};
+var result = add(5, 5);
+console.log(result);
---
+++ with-typescript.ts
@@ -0,0 +1,7 @@
+const add = (a: number, b: number) => {
+  console.log(a + b);
+};
+
+const result = add(5, 5);
+
+console.log(result)

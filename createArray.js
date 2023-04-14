72687d294493ae1f934b88690ad4fad7ea990656
Dan Stronge <dnlstronge@gmail.com>
2023-04-14 14:01:31 +0100

    [x] - demo creates an array of objects

---
+++ createArray.js
@@ -0,0 +1,16 @@
+var myObject; // object with an array of defined object types
+myObject = [
+    {
+        name: "dave",
+        phone: 12345,
+    },
+    {
+        name: "Barney",
+        phone: 43234,
+    },
+    {
+        name: "Norman",
+        phone: 6666234,
+    }
+];
+console.log(myObject);
---
+++ createArray.ts
@@ -0,0 +1,22 @@
+let myObject: {
+    name: string,
+    phone: number
+}[] // object with an array of defined object types
+
+myObject = [
+    
+    {
+        name: "dave",
+        phone: 12345,
+    },
+    {
+        name: "Barney",
+        phone: 43234,
+    },
+    {
+        name: "Norman",
+        phone: 6666234,
+    }
+]
+
+console.log(myObject)

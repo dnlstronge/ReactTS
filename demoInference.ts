516214a080556f588a39de753d55a602e5f2f4c3
Dan Stronge <dnlstronge@gmail.com>
2023-04-14 14:20:26 +0100

    [x] - demo basic inference and union types

--- demoInference.ts
+++ demoInference.ts
@@ -6,3 +6,25 @@ inferred = "12345" // will work because the type is inferred when initialized
 /* Typescript will try to infer as many types as possible
 
 EMBRACE INFERANCE!!!!*/
+
+let variable = "B"
+if (variable === "A") {
+    let showInference = ""
+    showInference = "I am a string";
+} else {
+    let showInference = 0
+    showInference = 12345
+}
+
+// or: 
+
+
+let showInferenceTwo: number | string;  
+    showInferenceTwo = 1234; // both work because of union
+    showInferenceTwo = "string";
+
+
+
+
+
+

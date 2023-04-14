c29201139a81f27caa384ca5fb8d93bd7694fd2c
Dan Stronge <dnlstronge@gmail.com>
2023-04-14 13:41:25 +0100

    [x] - add example

--- morebasics.ts
+++ morebasics.ts
@@ -1,9 +1,9 @@
 let mixedObject: {
     name: string,
-    phone: number,
+    phone: number | string,
 }
 
-const updateMixedObject = (a: string, b: number) => {
+const updateMixedObject = (a: string, b: any) => {
    mixedObject.name = a
    mixedObject.phone = b
    console.log("mixed object updated")
@@ -11,3 +11,9 @@ const updateMixedObject = (a: string, b: number) => {
 
 updateMixedObject("dave", 12345); // this works
 updateMixedObject("dave", "12345") // wont work
+
+const updateNew = (a: string, b: string) => {
+    mixedObject.name = a
+    mixedObject.phone = b
+    console.log("mixed object updated")
+} // will work

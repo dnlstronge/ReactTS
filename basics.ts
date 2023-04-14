70756beeb61caaf58feed165973eb7271675bb6d
Dan Stronge <dnlstronge@gmail.com>
2023-04-14 13:13:07 +0100

    [x] - play about with types

---
+++ basics.ts
@@ -0,0 +1,21 @@
+// primitives: strings, numbers, booleans,
+// more complex: arrays, objects
+// function types, parameters and more+
+
+
+// primitives: 
+
+let age: number;
+
+age = 45 // will work
+age = "FortyFive" // will not
+
+let userName: string;
+
+userName = "42142" // will work
+userName = 42142 // wont work
+
+let answer: boolean;
+
+answer = true; // will work
+answer = "true"; // wont work

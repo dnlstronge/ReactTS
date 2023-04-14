0d57ad84ac3f438583e4502d6e6764db1bf5bc1c
Dan Stronge <dnlstronge@gmail.com>
2023-04-14 14:08:07 +0100

    [x] - add comments

--- demoInference.ts
+++ demoInference.ts
@@ -2,3 +2,7 @@ let inferred = "windows"
 
 inferred = 12345; // wont work
 inferred = "12345" // will work because the type is inferred when initialized
+
+/* Typescript will try to infer as many types as possible
+
+EMBRACE INFERANCE!!!!*/

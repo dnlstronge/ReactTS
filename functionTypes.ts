4fc70c71d64e61d982cbf3dce78b3738865294e1
Dan Stronge <dnlstronge@gmail.com>
2023-04-15 09:23:59 +0100

    [x] - add new example

--- functionTypes.ts
+++ functionTypes.ts
@@ -16,12 +16,20 @@ function addAgain(a: number, b: number): number {
 
 /*Functions generally have an input and an output, hence they have a return type */
 
-
 /*Demo void - comparable to null & undefined
 funtion never returns */
 
 function printSomething(a: any) {
-    console.log(a)
+  console.log(a);
 }
 
 /* GENERICS */
+
+function insertAt(array: any[], value: any) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1, 2, 3];
+
+const updatedArray = insertAt(demoArray, -1); // doesn't pick up that it's an array of numbers

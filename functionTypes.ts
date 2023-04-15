89d024c8019d3a85b2a94011d8507faf1d233f06
Dan Stronge <dnlstronge@gmail.com>
2023-04-15 09:12:53 +0100

    [x] - add example [x] - add comments

--- functionTypes.ts
+++ functionTypes.ts
@@ -1,7 +1,15 @@
 /* function & types */
 
 function add(a: number, b: number) {
-    let answer = a + b 
-    console.log(answer)
-    return answer;// this is returned type (hover over add to see this)
+  let answer = a + b;
+  console.log(answer);
+  return answer; // this is returned type (hover over add to see this) - inferred return type (number)
+}
+
+// same as:
+
+function addAgain(a: number, b: number): number {
+  let answer = a + b;
+  console.log(answer);
+  return answer;
 }
